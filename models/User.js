2d79e8c3156410502ddf8afa5bb1dfec720b7ab7
JaacielBriseo <jaacielbris23@gmail.com>
2023-02-24 19:40:52 -0700

    Change on User model
    More properties added for more features

--- models/User.js
+++ models/User.js
@@ -3,16 +3,32 @@ const { Schema, model } = require('mongoose');
 const UserSchema = Schema({
 	name: {
 		type: String,
-		require: true,
+		required: [true, 'Name is required.'],
 	},
 	email: {
 		type: String,
-		require: true,
+		required: [true, 'Email is required.'],
 		unique: true,
 	},
 	password: {
 		type: String,
-		require: true,
+		required: [true, 'Password is required.'],
+	},
+	img: {
+		type: String,
+	},
+	role: {
+		type: String,
+		required: true,
+		emun: ['ADMIN_ROLE', 'USER_ROLE', 'SUPER_ADMIN'],
+	},
+	isActive: {
+		type: Boolean,
+		default: true,
+	},
+	google: {
+		type: Boolean,
+		default: false,
 	},
 });
 
